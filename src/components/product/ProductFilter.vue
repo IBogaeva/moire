f12c45d25c22933b805d74df6aa7251486460e8d
IBogaeva <ibogaeva@sportmaster.ru>
2021-04-12 15:13:43 +0300

    productFilter

--- src/components/product/ProductFilter.vue
+++ src/components/product/ProductFilter.vue
@@ -6,31 +6,33 @@
       <fieldset class="form__block">
         <legend class="form__legend">Цена</legend>
         <label class="form__label form__label--price">
-          <input class="form__input" type="text" name="min-price" v-model.number="currentPriceFrom">
+          <input class="form__input" type="text" name="min-price"
+                 v-model.number="filters.currentPriceFrom">
           <span class="form__value">От</span>
         </label>
         <label class="form__label form__label--price">
-          <input class="form__input" type="text" name="max-price" v-model.number="currentPriceTo">
+          <input class="form__input" type="text" name="max-price"
+                 v-model.number="filters.currentPriceTo">
           <span class="form__value">До</span>
         </label>
       </fieldset>
 
-      <fieldset class="form__block">
+      <fieldset class="form__block" v-if="categories">
         <legend class="form__legend">Категория</legend>
         <label class="form__label form__label--select">
-          <OptionList :list="categories" :current-id.sync="currentCategoryId">
+          <OptionList :list="categories" :current-id.sync="filters.currentCategoryId">
             <option value="0">Все категории</option>
           </OptionList>
         </label>
       </fieldset>
 
-      <fieldset class="form__block">
+      <fieldset class="form__block" v-if="colors">
         <legend class="form__legend">Цвет</legend>
         <ul class="colors colors--black">
           <li class="colors__item"  v-for="item in colors" :key="item.id">
             <label class="colors__label">
               <input type="checkbox" class="colors__radio sr-only"
-                     :value="item.id" v-model="currentColorIds">
+                     :value="item.id" v-model="filters.currentColorIds">
               <span class="colors__value" :style="'background-color: ' + item.code + ';'">
                   </span>
             </label>
@@ -38,21 +40,21 @@
         </ul>
       </fieldset>
 
-      <fieldset class="form__block">
+      <fieldset class="form__block" v-if="materials">
         <legend class="form__legend">Материал</legend>
-        <CheckList :current-ids.sync="currentMaterialIds" :list="materials"/>
+        <CheckList :current-ids.sync="filters.currentMaterialIds" :list="materials"/>
       </fieldset>
 
-      <fieldset class="form__block">
+      <fieldset class="form__block" v-if="seasons">
         <legend class="form__legend">Коллекция</legend>
-        <CheckList :current-ids.sync="currentSeasonIds" :list="seasons"/>
+        <CheckList :current-ids.sync="filters.currentSeasonIds" :list="seasons"/>
       </fieldset>
 
       <button class="filter__submit button button--primery" type="submit">
         Применить
       </button>
       <button class="filter__reset button button--second" type="button" @click.prevent="reset"
-              v-show="isFilterSet">
+              v-if="isFilterSet">
         Сбросить
       </button>
     </form>
@@ -64,6 +66,16 @@ import { mapGetters, mapActions } from 'vuex';
 import CheckList from '@/components/common/CheckList.vue';
 import OptionList from '@/components/common/OptionList.vue';
 import Loader from '@/components/common/Loader.vue';
+import isEqual from 'lodash/isEqual';
+
+const INITIAL_FILTERS = {
+  currentCategoryId: 0,
+  currentPriceFrom: 0,
+  currentPriceTo: 0,
+  currentMaterialIds: [],
+  currentSeasonIds: [],
+  currentColorIds: [],
+};
 
 export default {
   props: ['priceFrom', 'priceTo', 'categoryId', 'colorIds', 'materialIds', 'seasonIds'],
@@ -72,12 +84,7 @@ export default {
   },
   data() {
     return {
-      currentCategoryId: 0,
-      currentPriceFrom: 0,
-      currentPriceTo: 0,
-      currentMaterialIds: [],
-      currentSeasonIds: [],
-      currentColorIds: [],
+      filters: { ...INITIAL_FILTERS },
     };
   },
   computed: {
@@ -89,22 +96,17 @@ export default {
       isUiLocked: 'isUiLocked',
     }),
     isFilterSet() {
-      return this.currentCategoryId > 0
-        || this.currentPriceFrom > 0
-        || this.currentPriceTo > 0
-        || this.currentMaterialIds.length > 0
-        || this.currentSeasonIds.length > 0
-        || this.currentColorIds.length > 0;
+      return !isEqual(this.filters, INITIAL_FILTERS);
     },
   },
   methods: {
     submit() {
-      this.$emit('update:priceFrom', this.currentPriceFrom);
-      this.$emit('update:priceTo', this.currentPriceTo);
-      this.$emit('update:categoryId', this.currentCategoryId);
-      this.$emit('update:colorIds', this.currentColorIds);
-      this.$emit('update:seasonIds', this.currentSeasonIds);
-      this.$emit('update:materialIds', this.currentMaterialIds);
+      this.$emit('update:priceFrom', this.filters.currentPriceFrom);
+      this.$emit('update:priceTo', this.filters.currentPriceTo);
+      this.$emit('update:categoryId', this.filters.currentCategoryId);
+      this.$emit('update:colorIds', this.filters.currentColorIds);
+      this.$emit('update:seasonIds', this.filters.currentSeasonIds);
+      this.$emit('update:materialIds', this.filters.currentMaterialIds);
     },
     reset() {
       this.$emit('update:priceFrom', 0);
@@ -113,12 +115,7 @@ export default {
       this.$emit('update:colorIds', []);
       this.$emit('update:seasonIds', []);
       this.$emit('update:materialIds', []);
-      this.currentPriceFrom = 0;
-      this.currentPriceTo = 0;
-      this.currentCategoryId = 0;
-      this.currentMaterialIds = [];
-      this.currentSeasonIds = [];
-      this.currentColorIds = [];
+      this.filters = { ...INITIAL_FILTERS };
     },
     ...mapActions([
       'loadCategories',
